7d5dfa9fb4a5441fdb39168621fb2d2ffdabe849
Maciej Lewinski <maciej.lewinski@mobica.com>
2019-03-12 14:27:42 +0100

    Updates

---
+++ precache-manifest.5db6ed4ffca2f6451e5ef7d72b07bfd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dd5731105d0c6e0cd8e",
+    "url": "/rosi/static/js/runtime~main.5dd57311.js"
+  },
+  {
+    "revision": "69d890aef5a0a394104a",
+    "url": "/rosi/static/js/main.69d890ae.chunk.js"
+  },
+  {
+    "revision": "7fdf83d4b5f2757487ff",
+    "url": "/rosi/static/js/1.7fdf83d4.chunk.js"
+  },
+  {
+    "revision": "69d890aef5a0a394104a",
+    "url": "/rosi/static/css/main.a455b2dc.chunk.css"
+  },
+  {
+    "revision": "a59d9eb826c2dcb3ccfb869993a461bc",
+    "url": "/rosi/index.html"
+  }
+];
