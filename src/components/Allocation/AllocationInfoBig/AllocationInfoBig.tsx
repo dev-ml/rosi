eadeec17cf5f12a9c020b24c554c2872f0f65f66
Maciej Lewinski <maciej.lewinski@mobica.com>
2019-03-26 14:52:34 +0100

    fix: calculation of time information for allocation information big

--- src/components/Allocation/AllocationInfoBig/AllocationInfoBig.tsx
+++ src/components/Allocation/AllocationInfoBig/AllocationInfoBig.tsx
@@ -3,6 +3,11 @@ import { formatTimeStamp, millisecondsToMinutes } from "../../../shared/utility"
 import "./AllocationInfoBig.scss";
 
 const allocationInfoBig = (props: any) => {
+  const timeTillEnd = millisecondsToMinutes(props.to - props.time);
+  const duration = millisecondsToMinutes(props.to - props.from);
+  const timeInMeeting = duration - timeTillEnd;
+  const percentInMeeting = Math.round(timeInMeeting / duration * 100);
+
   return (
     <>
       <div className="AllocationInfoBig">Current Meeting:
@@ -11,9 +16,9 @@ const allocationInfoBig = (props: any) => {
         <div>{props.title}</div>
         {props.attendees ? <div>Attendees: {props.attendees}</div> : null}
         {props.agenda ? <div>Agenda: {props.agenda}</div> : null}
-        <div>Duration: {millisecondsToMinutes(props.to - props.from)} min</div>
-        <div>Time till end: {millisecondsToMinutes(props.to - props.time)} min</div>
-        <div>% Done: {100 - Math.ceil(100 * (props.to - props.time) / (props.to - props.from))}</div>
+        <div>Duration: {duration} min</div>
+        <div>Time till end: {timeTillEnd} min</div>
+        <div>% Done: {percentInMeeting}</div>
       </div>
     </>
   );
